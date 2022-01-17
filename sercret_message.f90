efca3dc14547aa288bf454894ed6944a86c21f6e
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-16 21:59:33 -1000

    Add files via upload

---
+++ sercret_message.f90
@@ -0,0 +1,8 @@
+program sercret_message
+  implicit none
+
+  print *, &
+    char (87) // char (69) // char (76) // char (67) &
+    // char (79) // char (77) // char (69)
+
+end program sercret_message
